dd12ab33f7940a352f007dd6329ea011eaf57cc4
Pawel Andruszkiewicz <p.andruszkie@samsung.com>
2015-10-30 15:57:23 +0100

    [lib] Added console plugin (doesn't require native implementation).
    Change-Id: Ide496225d80f7fd00f3bc832eef2beb9cecc1653
    Signed-off-by: Pawel Andruszkiewicz <p.andruszkie@samsung.com>

--- src/lib/cordova_plugins.js
+++ src/lib/cordova_plugins.js
@@ -1,6 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
         "clobbers": [
@@ -185,6 +199,7 @@ module.exports = [
 module.exports.metadata =
 // TOP OF METADATA
 {
+    "cordova-plugin-console": "1.0.1",
     "cordova-plugin-device": "1.0.1",
     "cordova-plugin-dialogs": "1.1.1",
     "cordova-plugin-file": "3.0.0",
